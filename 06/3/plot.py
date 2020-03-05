ecb8e97973f803ffe4aea1cd02c3f6dba498a191
Alexander Harnisch <alexanderharnisch@gmail.com>
2020-03-05 09:42:21 -0500

    Corrected mistake in Homework 6: The matrices were generated wrongly. However, I did not run the script through with full statistics again...

--- 06/3/plot.py
+++ 06/3/plot.py
@@ -15,25 +15,25 @@ def p_factorized(x):
     return (x - 2)**9
 
 
-x = np.linspace(-1920, 2080, num=(2080 + 1920), dtype=np.float32)
+x = np.linspace(-1920, 2080, num=(2080 + 1920)*100, dtype=np.float32)
 
 plt.plot(x, p_coef(x), '.', ms=.5)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$p_{\mathrm{coef}}(x)$')
 # plt.show()
-plt.savefig('a.pdf')
+plt.savefig('a.png')
 plt.clf()
 
 plt.plot(x, p_factorized(x), '.', ms=.5)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$p_{\mathrm{fact}}(x)$')
 # plt.show()
-plt.savefig('b.pdf')
+plt.savefig('b.png')
 plt.clf()
 
 plt.plot(x, p_factorized(x) - p_coef(x), '.', ms=.5)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$p_{\mathrm{fact}}(x) - p_{\mathrm{coef}}(x)$')
 # plt.show()
-plt.savefig('a_b_diff.pdf')
+plt.savefig('a_b_diff.png')
 plt.clf()
